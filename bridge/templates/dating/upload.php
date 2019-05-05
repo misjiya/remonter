5eca258c85eb333bfa37fdd11e41205a215df919
misbah <4umisbah@gmail.com>
2019-05-05 21:56:02 +0530

    code4

--- bridge/templates/dating/upload.php
+++ bridge/templates/dating/upload.php
@@ -2,7 +2,57 @@
 <body>
 <?php include_once(INC.'nav.php');?>
 <div class="content-body">
+    <div class="upload-container">
+        
+        <div class="upl-ins-blck">
+            <div class="upg-title"><h3 class="t-center">GUIDELINES</h3></div>
+            <p class="msg">Upload your photos to get more focused here and get ready to meet new people.</p>
+            <ul class="upg-gline">
+                <li>Your own photos</li>
+                <li>Good quality photos</li>
+                <li>Ignore obscene photos</li>
+                <li>Keep changing your photos</li>
+            </ul>
+            <p class="sul">We will keep you photos safe and secure, only visible to recommended profiles</p>
+        </div>
+        <div class="upg-photo-blck">
+            <div class="upg-in">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="upg-photos"><span>+</span></div>
+                    </div>
+                    <div class="col-4">
+                        <div class="upg-photos"><span>+</span></div>
+                    </div>
+                    <div class="col-4">
+                        <div class="upg-photos"><span>+</span></div>
+                    </div>
+                </div> 
+                <div class="row">
+                    <div class="col-4">
+                        <div class="upg-photos"><span>+</span></div>
+                    </div>
+                    <div class="col-4">
+                        <div class="upg-photos"><span>+</span></div>
+                    </div>
+                    <div class="col-4">
+                        <div class="upg-photos"><span>+</span></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3 mrtop-15">
+                        <div class="btn-defalut">Skip</div>
+                    </div>
+                    <div class="col-9 mrtop-15">
+                        <div class="signup-btn-reg">Proceed</div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>  
+    
 </div>
-<?php include_once(INC.'footer.php');?>
+<?//php include_once(INC.'footer.php');?>
 </body>
 </html>
